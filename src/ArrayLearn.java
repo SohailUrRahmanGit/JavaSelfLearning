b8dfc4d6ccc17d70aae9c9ad6501b0cb9779eb26
soshaik <ssohailmails@gmail.com>
2024-07-11 19:30:33 +0530

    Array Learn 2

--- src/ArrayLearn.java
+++ src/ArrayLearn.java
@@ -1,6 +1,6 @@
 public class ArrayLearn {
     public static void main(String[] args) {
-        String [] cities = { "Chennai", "Hyderabad", "Kerala", "Bangalore" };
+        String [] cities = { "Chennai", "Hyderabad", "Kerala", "Bangalore", "Coimbatore" };
         System.out.println(cities[0]);
         System.out.println(cities[1]);
         System.out.println(cities[2]);
