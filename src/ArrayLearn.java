aec69fca79b79d33ab4dbf4a04cf4e0004f672e9
soshaik <soshaik@paypal.com>
2024-07-11 19:28:51 +0530

    Array Learn

---
+++ src/ArrayLearn.java
@@ -0,0 +1,8 @@
+public class ArrayLearn {
+    public static void main(String[] args) {
+        String [] cities = { "Chennai", "Hyderabad", "Kerala", "Bangalore" };
+        System.out.println(cities[0]);
+        System.out.println(cities[1]);
+        System.out.println(cities[2]);
+    }
+}
